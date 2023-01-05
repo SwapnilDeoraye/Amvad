0f6f669c11d5c20d48b313483851a2e0a530fdc1
SwapnilDeoraye <91518838+SwapnilDeoraye@users.noreply.github.com>
2023-01-05 12:58:06 +0530

    Changes1ok

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public int add(int a, int b){
-        int c = a+b;
+        int c = a + b;
         System.out.println("Good");
         return c;
     }
@@ -11,7 +11,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I can do it");
         Main main = new Main();
-        int d = main.add(12,12);
+        int d = main.add(12, 12);
         System.out.println(d);
+
+        int f = main.add(222, 12);
+        System.out.println(f);
+    }
+
+    public int add1(int a, int b) {
+        int c = a + b;
+        System.out.println("Good");
+        return c;
     }
 }
