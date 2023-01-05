916768689acb9e0595848672adc31c5c05901f41
SwapnilDeoraye <91518838+SwapnilDeoraye@users.noreply.github.com>
2023-01-05 12:10:54 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public int add(int a, int b){
+        int c = a+b;
+        System.out.println("Good");
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I can do it");
+        Main main = new Main();
+        int d = main.add(12,12);
+        System.out.println(d);
+    }
+}
